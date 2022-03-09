f098f5af613d25a298da04e449bf8ef05e0c6356
Simmo Saan <simmo.saan@gmail.com>
2022-03-09 23:04:31 +0200

    Clean up and simplify Mimram typing example

--- example/mimram/typing.ml
+++ example/mimram/typing.ml
@@ -1,10 +1,4 @@
-(* let inc x = x + 1
-
-let _ =
-  match 5 with
-  | [%view? 6 when inc] -> 6
-  (* | n -> n *)
-  (* | [%view? k when inc] -> k *) *)
+(** https://www.lix.polytechnique.fr/Labo/Samuel.Mimram//teaching/INF551/TD/1.typing.html *)
 
 type prog =
   | Bool of bool
@@ -12,10 +6,6 @@ type prog =
   | Add of prog * prog
   | Lt of prog * prog
   | If of prog * prog * prog
-  | Pair of prog * prog
-  | Fst of prog
-  | Snd of prog
-  | Unit
 
 let rec reduce = function
   | Add (Int n1, Int n2) -> Some (Int (n1 + n2))
@@ -27,12 +17,6 @@ let rec reduce = function
   | If (Bool true, p1, _p2) -> Some p1
   | If (Bool false, _p1, p2) -> Some p2
   | If ([%view? Some p' when reduce], p1, p2) -> Some (If (p', p1, p2))
-  | Pair ([%view? Some p1' when reduce], p2) -> Some (Pair (p1', p2))
-  | Pair (p1, [%view? Some p2' when reduce]) -> Some (Pair (p1, p2'))
-  | Fst (Pair (p1, _p2)) -> Some p1
-  | Fst [%view? Some p' when reduce] -> Some (Fst p')
-  | Snd (Pair (_p1, p2)) -> Some p2
-  | Snd [%view? Some p' when reduce] -> Some (Snd p')
   | _ -> None
 
 let rec normalize p =
@@ -40,41 +24,41 @@ let rec normalize p =
   | Some p' -> normalize p'
   | None -> p
 
+
+(* if 3 < 2 then 5 else 1 *)
+let%test _ = normalize (If (Lt (Int 3, Int 2), Int 5, Int 1)) = Int 1
 (* if 1+(2+3) < 4 then false else 5 *)
 let%test _ = normalize (If (Lt (Add (Int 1, Add (Int 2, Int 3)), Int 4), Bool false, Int 5)) = Int 5
 (* if 1+(2+3) < 10 then false else 5 *)
 let%test _ = normalize (If (Lt (Add (Int 1, Add (Int 2, Int 3)), Int 10), Bool false, Int 5)) = Bool false
-let%test _ = normalize (Pair (Snd (Pair (Int 3, Int 2)), Fst (Pair (Int 3, Int 2)))) = Pair (Int 2, Int 3)
+
 
 type typ =
   | TBool
   | TInt
-  | TPair of typ * typ
-  | TUnit
 
 exception Type_error
 
 let rec infer = function
-  | Int _ -> TInt
   | Bool _ -> TBool
+  | Int _ -> TInt
   | Add ([%view? TInt when infer], [%view? TInt when infer]) -> TInt
   | Lt ([%view? TInt when infer], [%view? TInt when infer]) -> TBool
-  | If ([%view? TBool when infer], p1, p2) when infer p1 = infer p2 -> infer p1
-  | Pair (p1, p2) -> TPair (infer p1, infer p2)
-  | Fst [%view? TPair (t1, _t2) when infer] -> t1
-  | Snd [%view? TPair (_t1, t2) when infer] -> t2
-  | Unit -> TUnit
+  | If ([%view? TBool when infer], [%view? t1 when infer], [%view? t2 when infer]) when t1 = t2 -> t1
   | _ -> raise Type_error
 
 let typable p = match infer p with
   | _ -> true
   | exception Type_error -> false
 
+
+(* if 3 < 2 then 5 else 1 *)
+let%test _ = typable (If (Lt (Int 3, Int 2), Int 5, Int 1))
+let%test _ = infer (If (Lt (Int 3, Int 2), Int 5, Int 1)) = TInt
 (* if (1 + 2) < 3 then 4 else 5 *)
 let%test _ = typable (If (Lt (Add (Int 1, Int 2), Int 3), Int 4, Int 5))
+let%test _ = infer (If (Lt (Add (Int 1, Int 2), Int 3), Int 4, Int 5)) = TInt
 (* 1 + false *)
 let%test _ = not (typable (Add (Int 1, Bool false)))
 (* if true then 1 else false *)
 let%test _ = not (typable (If (Bool true, Int 1, Bool false)))
-(* (1 , false) *)
-let%test _ = typable (Pair (Int 1, Bool false))
