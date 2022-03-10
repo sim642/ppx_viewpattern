8e691aa67875e864f85d4c030e7bd989bbceb04c
Simmo Saan <simmo.saan@gmail.com>
2022-03-10 22:58:02 +0200

    Qualify QCheck.Fun GADT for OCaml 4.06

--- example_test/haskell/iterator_style_test.ml
+++ example_test/haskell/iterator_style_test.ml
@@ -10,14 +10,14 @@ let test_length =
 
 let test_map =
   QCheck_ounit.to_ounit2_test (
-    QCheck.Test.make ~name:"map" QCheck.(pair (fun1 Observable.int int) (list int)) (fun (Fun (_, f), l) ->
+    QCheck.Test.make ~name:"map" QCheck.(pair (fun1 Observable.int int) (list int)) (fun (QCheck.Fun (_, f), l) ->
         Iterator_style.map f l = List.map f l
       )
   )
 
 let test_foldr =
   QCheck_ounit.to_ounit2_test (
-    QCheck.Test.make ~name:"foldr" QCheck.(triple (fun2 Observable.int Observable.int int) int (list int)) (fun (Fun (_, f), z, l) ->
+    QCheck.Test.make ~name:"foldr" QCheck.(triple (fun2 Observable.int Observable.int int) int (list int)) (fun (QCheck.Fun (_, f), z, l) ->
         Iterator_style.foldr f z l = List.fold_right f l z
       )
   )
